90ac76cdda6dace1e3ebc5b1ad2ec484dbe05a5a
Emvit <81639393+ErikMekiassen@users.noreply.github.com>
2023-07-18 23:08:19 +0200

    Initial commit

---
+++ src/main/java/com/emvit/learnJava/LearnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.emvit.learnJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emvit/learnJava/LearnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emvit.learnJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
